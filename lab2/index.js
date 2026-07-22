577c87e2b146d2d80464f16afbe7835b82bc1ff5
aditya74203 <aaditya.25b01010727@abes.ac.in>
2026-07-22 12:23:19 +0530

    22/07/26

---
+++ lab2/index.js
@@ -0,0 +1,29 @@
+const toWords =(digit)=>{
+    switch (digit){
+     case 1:
+            return "One";
+        case 2:
+            return "Two";
+        case 3:
+            return "Three";
+        case 4:
+            return "Four";
+        case 5:
+            return "Five";
+        case 6:
+            return "Six";
+        case 7:
+            return "Seven";
+        case 8:
+            return "Eight";
+        case 9:
+            return "Nine";
+        case 10:
+            return "Ten";
+    };
+}
+
+
+console.log(toWords(5)); 
+console.log(toWords(10)); 
+console.log(toWords(15));4
